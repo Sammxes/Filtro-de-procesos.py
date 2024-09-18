dffc8bb65d0e1cecae25a5e3396266dd924e2113
smnx <138089765+Sammxes@users.noreply.github.com>
2024-09-17 21:39:30 -0700

    Create code.py
    linea para filtrar procesos con py.

---
+++ code.py
@@ -0,0 +1,17 @@
+import psutil
+
+def listar_procesos():
+    for proceso in psutil, process_iter([`pid`, `Name`, `status`]):
+ try: 
+print(f*PID:
+ {proceso.info[`pid`]}, Name:
+                  {proceso.info[`name`]}, Estado:
+                  {proceso.info[`status`]}")
+                  except
+                  (psutil.NoSuchProcess,
+                   psutil.AccessDenied):
+                   continue     
+                   listar_procesos()
+                   )
+
+
